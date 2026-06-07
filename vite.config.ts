0e22767fe9bb2b9f5f233a449bd58843be5763d4
Thanh Thuy Huynh <hthuy03.work@gmail.com>
2026-06-08 01:45:41 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/layerproof-prototype/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+    dedupe: ['react', 'react-dom'],
   },
 })
